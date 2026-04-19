b0f569d9ab10dfb05d8b365c9b122dd7143928e7
eloi <izabayoeloi2@gmail.com>
2026-04-19 05:35:25 +0200

    Finalising my portfolio

--- portfolio/src/app/components/skills/skills.ts
+++ portfolio/src/app/components/skills/skills.ts
@@ -13,8 +13,11 @@ export class Skills {
   constructor(public theme: ThemeService) {}
 
   skills = [
-    { name: 'Java', level: 75, icon: '🐍' },
-    { name: 'HTML / CSS', level: 90, icon: '🎨' },
-    { name: 'SQL / Databases', level: 70, icon: '🗄️' },
+    { name: 'Java', level: 75, icon: '/images/java.png' },
+    { name: 'HTML / CSS', level: 90, icon: '/images/html.png' },
+    { name: 'SQL / Databases', level: 70, icon: '/images/database.png' },
+    { name: 'JAVA-SWING', level:70, icon: '/images/swing.png'},
+    { name: 'Spring boot', level:60, icon: '/images/spring.png'},
+    { name: 'C', level:50, icon: '/images/c.png'}
   ];
 }
